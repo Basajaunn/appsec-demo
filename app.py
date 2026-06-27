402523a9444cec576e49e1fc4aa9a894deb3e365
Basajaunn <osodelatierra@gmail.com>
2026-06-27 08:15:29 -0400

    Started db and routing

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "APPSEC WOOOH"
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    
+
+@app.route('/user/<username>')
+def user(username):
+    return f'Hello {username} !'
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+def get_db():
+    con = sqlite3.connect("database.db")
+    cur = connect.cursor()
+    
+
+def get_username():
