ce25bf76d382d86372730fed61f7053687abded5
Basajaunn <osodelatierra@gmail.com>
2026-07-01 12:05:43 -0400

    Push before vacation, WIP

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, flash, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 import db
 
 app = Flask(__name__)
+app.secret_key = "super-secure-secret-key"
 
 @app.route("/")
 def home():
@@ -22,9 +23,23 @@ def register():
     return render_template('register.html')
     
 
-@app.route('/user/<username>')
-def user(username):
-    return f'Hello {username} !'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = db.get_user(username)
+
+        if user and check_password_hash(user["password_hash"], password):
+            session['username'] = user['username']
+            flash('Login was successful')
+            return redirect(url_for('homepage.html'))
+        else:
+            return("Failed login")
+
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
--- db.py
+++ db.py
@@ -1,12 +1,16 @@
 import sqlite3
 
 def get_db():
-    return sqlite3.connect("database.db")
+    connect = sqlite3.connect("database.db")
+    connect.row_factory = sqlite3.Row
+    return connect
 
 def create_user(username, password_hash):
     connect = get_db()
     cursor = connect.cursor()
+
     cursor.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", (username, password_hash))
+    
     connect.commit()
     connect.close()
 
